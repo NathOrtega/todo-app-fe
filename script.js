84f8e611baca75d68144ba4a7e3cff612702359e
Nath Ortega <nath_0563@hotmail.com>
2021-08-29 16:57:08 -0500

    Adding logic to detect user color scheme preferences

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const modeButton=document.getElementById("modeIconButton");
-const modeIcon=document.getElementById("modeIcon");
+const modeButton = document.getElementById("modeIconButton");
+const modeIcon = document.getElementById("modeIcon");
+const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+function changeThemeIcon(){
+  if (document.body.classList.contains("darkMode")) {
+    modeIcon.src = "./Resources/icon-sun.svg";
+  } else {
+    modeIcon.src = "./Resources/icon-moon.svg";
+  }
+}
 
 modeButton.onclick = () => {
   document.body.classList.toggle("darkMode");
-  if (document.body.classList.contains("darkMode")){
-    modeIcon.src="./Resources/icon-sun.svg";
-  } else {
-    modeIcon.src="./Resources/icon-moon.svg";
-  }
+  changeThemeIcon();
+}
+
+if (prefersDarkMode) {
+  document.body.classList.add("darkMode");
+  changeThemeIcon()
+} else {
+  document.body.classList.remove("darkMode");
+  changeThemeIcon()
 }
