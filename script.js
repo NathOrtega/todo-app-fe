69e8e9369806f598338e3741502a1ba0f79e7619
Nath Ortega <nath_0563@hotmail.com>
2021-08-30 20:12:56 -0500

    Starting to write logic to add a new toDo and to show all toDos

--- script.js
+++ script.js
@@ -1,6 +1,68 @@
 const modeButton = document.getElementById("modeIconButton");
 const modeIcon = document.getElementById("modeIcon");
 const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const checkButtons = Array.from(document.getElementsByClassName("checkButton"));
+const toDoInput = document.getElementById("toDoInput");
+
+checkButtons.forEach(button => {
+  button.onclick = () => {
+    button.classList.toggle("active");
+  }
+});
+
+toDoInput.onkeypress = keyPressed => {
+  if (keyPressed.key === "Enter"){
+    if (toDoInput.value.trim() !== ""){
+      const toDo = toDoInput.value;
+      const checkButton=toDoInput.previousElementSibling.firstElementChild;
+      const isCompleted=checkButton.classList.contains("active");
+      postNewToDo(toDo, isCompleted);
+      getAllToDos();
+      toDoInput.value=null
+    }
+  }
+}
+
+async function postNewToDo(toDo, isCompleted){
+  try {
+    await fetch("https://todos-app-be.herokuapp.com/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify ({
+        "description": toDo,
+        "isCompleted": isCompleted
+      })
+    })
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+async function getAllToDos(){
+  const allToDosContainer=document.getElementById("allToDosContainer");
+  try {
+    const toDos=await fetch("https://todos-app-be.herokuapp.com/todos");
+    const toDosJson=await toDos.json()
+    toDosJson.map(toDo => {
+      allToDosContainer.innerHTML+=`
+      <div class="toDo">
+      <div class="checkButtonBorder">
+        <button class="checkButton">
+          <img src="./Resources/icon-check.svg" alt="">
+        </button>
+      </div>
+      <p class="text">${toDo.description}</p>
+      <button class="deleteButton" id="deleteButton">
+        <img src="./Resources/icon-cross.svg" alt="Delete Cross">
+      </button>
+      `
+    })
+  } catch(error){
+    console.log(error)
+  }
+}
 
 function changeThemeIcon(){
   if (document.body.classList.contains("darkMode")) {
@@ -17,8 +79,5 @@ modeButton.onclick = () => {
 
 if (prefersDarkMode) {
   document.body.classList.add("darkMode");
-  changeThemeIcon()
-} else {
-  document.body.classList.remove("darkMode");
   changeThemeIcon()
 }
