e1fa377003df93762e58e01213f456ea86563658
Nath Ortega <nath_0563@hotmail.com>
2021-09-06 09:22:08 -0500

    add hover state to the modeIcon and the deleteButton, add logic to save user's mode preference with localStorage

--- script.js
+++ script.js
@@ -192,7 +192,7 @@ function createTaskTemplate(isCompleted, id, description) {
   </div>
   <p class="text ${isCompleted ? "completed" : ""}" id="${id}">${description}</p>
   <button class="deleteButton">
-    <img src="./Resources/icon-cross.svg" alt="Delete Cross">
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path class="deleteIcon" fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
   </button>
   `
   return template;
@@ -295,20 +295,40 @@ function toggleCompletedClass(text) {
 }
 
 function changeThemeIcon() {
-  const modeIcon = document.getElementById("modeIcon");
   if (document.body.classList.contains("darkMode")) {
-    modeIcon.src = "./Resources/icon-sun.svg";
+    modeButton.innerHTML=`
+    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path class="modeIcon" fill="#FFF" fill-rule="evenodd" d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z"/></svg>
+    `
   } else {
-    modeIcon.src = "./Resources/icon-moon.svg";
+    modeButton.innerHTML=`
+    <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path class="modeIcon" fill="#FFF" fill-rule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"/></svg>
+    `
+  }
+}
+
+function saveUserThemePreferences(){
+  if (document.body.classList.contains("darkMode")){
+    localStorage.setItem("darkMode", "true");
+  } else {
+    localStorage.setItem("darkMode", "false");
   }
 }
 
 modeButton.onclick = () => {
   document.body.classList.toggle("darkMode");
+  saveUserThemePreferences();
   changeThemeIcon();
 }
 
 if (prefersDarkMode) {
   document.body.classList.add("darkMode");
   changeThemeIcon()
+}
+
+if (localStorage.getItem("darkMode") === "true"){
+  document.body.classList.add("darkMode");
+  changeThemeIcon();
+} else {
+  document.body.classList.remove("darkMode");
+  changeThemeIcon();
 }
